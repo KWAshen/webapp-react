9b0e607a4c1aa27c3d25d9e417011627984f24b6
KWAshen <ashen.maliaha@gmail.com>
2025-09-26 11:19:19 +0200

    cambio title , correzione css, correzione generali, layout non pushata prima

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 d-flex justify-content-between align-items-center">
+            <h1>Logo</h1>
+            <ul className="list-unstyled d-flex m-3">
+              <li className='me-3'><NavLink to="/">Home</NavLink></li>
+              <li className='me-3'><NavLink to="/movies">Movies</NavLink></li>
+            </ul>
+            <div>
+              <Link to="/movies" className='btn btn-turn-list'>Torna alla lista</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
